881eedc55e00d7e8a805a31c817df890dddf824a
alexruedadev <me@alexrueda.dev>
2023-01-05 18:23:44 +0100

    Some changes

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,69 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import React, { useState } from "react";
 
-export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+export default function App() {
+
+  const [img, setImg] = useState();
+  const [imageToEdit, setImageToEdit] = useState();
+  const [portrait, setPortait] = useState();
+
 
   async function onSubmit(event) {
     event.preventDefault();
+
+    // Comprobar si se ha subido la imagen.
+
+    const photoInput = img;
+    console.log(imageToEdit)
+    // generateImage
     try {
-      const response = await fetch("/api/generate", {
+      const response = await fetch("/api/generateImage", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ photo: imageToEdit }),
       });
 
       const data = await response.json();
+
+      console.log(data)
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
-    }
+  } catch(error) {
+    // Consider implementing your own error handling logic here
+    console.error(error);
+    alert(error.message);
   }
+}
+  const onImageChange = (e) => {
+
+    // Get the file.
+    const [file] = e.target.files;
+
+    /*
+    * Image validation.
+    ? Consult the API image documentation -> https://beta.openai.com/docs/guides/images
+    TODO: Check if is a single file and if is an image. If not, notify to user.
+    TODO: Check if the image is not too big or too small.
+    TODO: Maybe check the quality of the image and face position. If not is good quality, notify to user.
+    */
+    console.log(file);
+
+    // Create an object URL for the file.
+    setImg(URL.createObjectURL(file));
+    setImageToEdit(file);
+
+  };
 
   return (
     <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
-
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
+      <input type="file" onChange={onImageChange} accept="image/png, image/jpg" />
+      <img src={img} alt="" />
+      <form onSubmit={onSubmit}>
+        <input type="submit" value="Portrait me!"/>
+      </form>
     </div>
   );
 }
